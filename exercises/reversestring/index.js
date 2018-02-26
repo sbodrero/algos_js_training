99b3bbecf962c2545c6fb07405f0db9f90d6c99e
Bodrero Sébastien <sebastien@mediis.fr>
2018-02-26 14:02:10 +0100

    First commit pushed everything

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,37 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+// 	const arr = str.split('');
+// 	arr.reverse();
+// 	return arr.join('');	
+// }
+
+/* function reverse(str){
+	return str
+	.split('')
+	.reverse()
+	.join('')
+} */
+
+/* function reverse(str) {
+	let reversed = '';
+
+	for(let char of str) {
+		reversed = char + reversed;
+	}
+
+	return reversed;
+} */
+
+/* function reverse(str) {
+	return str.split('').reduce((rev, char) => {
+		return char + rev;
+	}, '')
+} */
+
+function reverse(str) {
+	return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
